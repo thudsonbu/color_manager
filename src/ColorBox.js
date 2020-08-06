4f806aa74e9a8b3c3860f96cda7f63869e9d1ad3
thudsonbu <hudson_thom@bentley.edu>
2020-08-06 14:00:29 -0600

    Copy animation and styling changes to palette

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,12 +3,33 @@ import { CopyToClipboard } from 'react-copy-to-clipboard'
 import './ColorBox.css';
 
 class ColorBox extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            copied: false,
+        }
+        this.changeCopyState = this.changeCopyState.bind(this)
+    }
+    changeCopyState(){
+        this.setState({copied: true}, () => {
+            setTimeout(() => this.setState({copied: false}), 1500)
+        })
+    }
     render() {
         const { name, background } = this.props; // unpack props
+        const { copied } = this.state;
         return (
-            <CopyToClipboard text={background}>
-                <div className="ColorBox" style={{backgroundColor: background}}>
-                    <div className="copy-container">
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div className="ColorBox">
+                    <div 
+                        className={`copy-overlay ${copied && "show"}`} 
+                        style={{backgroundColor: background}}>
+                    </div>
+                    <div className={`copy-msg ${copied && "show"}`}>
+                        <h1>Copied!</h1>
+                        <p>{background}</p>
+                    </div>
+                    <div className="copy-container" style={{backgroundColor: background}}>
                         <div className="box-content">
                             <span>{name}</span>
                         </div>
