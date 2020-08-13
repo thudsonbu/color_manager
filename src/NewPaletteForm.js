52901e35f03f063757a13d911d0d2e266983782b
thudsonbu <hudson_thom@bentley.edu>
2020-08-13 15:09:02 -0600

    Colors added succesfully

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import { ChromePicker } from "react-color";
+import { colors } from "@material-ui/core";
 
 const drawerWidth = 400;
 
@@ -73,9 +74,16 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
-    state = {
-        open: false
-    };
+    constructor(props){
+        super(props)
+        this.state = {
+            open: true,
+            currentColor: "teal",
+            colors: ["purple", "#fa0061"]
+        }
+        this.updateCurrentColor = this.updateCurrentColor.bind(this);
+        this.addNewColor = this.addNewColor.bind(this);
+    }
 
     handleDrawerOpen = () => {
         this.setState({ open: true });
@@ -84,6 +92,14 @@ class NewPaletteForm extends Component {
     handleDrawerClose = () => {
         this.setState({ open: false });
     };
+
+    updateCurrentColor(newColor){
+        this.setState({ currentColor: newColor.hex });
+    }
+
+    addNewColor() {
+        this.setState({colors: [...this.state.colors, this.state.currentColor ]})
+    }
 
     render() {
         const { classes } = this.props;
@@ -133,10 +149,17 @@ class NewPaletteForm extends Component {
                         <Button variant="contained" color="primary">Random Color</Button>
                     </div>
                     <ChromePicker
-                        color='purple'
-                        oncChangeComplete={newColor => console.log(newColor)}
+                        color={this.state.currentColor}
+                        onChangeComplete={this.updateCurrentColor}
                     />
-                    <Button variant="contained" color="primary">Add Color</Button>
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        style={{backgroundColor: this.state.currentColor}} 
+                        onClick={this.addNewColor}
+                    >
+                        Add Color
+                    </Button>
                 </Drawer>
                 <main
                     className={classNames(classes.content, {
