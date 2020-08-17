6f9d655e69378a1e1952bef900b0588a310f125e
thudsonbu <hudson_thom@bentley.edu>
2020-08-17 13:51:18 -0600

    Reverted to pre-transition state

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import SingleColorPalette from './SingleColorPalette'
 import NewPaletteForm from './NewPaletteForm'
 import { generatePalette } from './colorHelpers'
 import PaletteList from './PaletteList';
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import Page from "./Page";
 
 class App extends Component {
   constructor(props) {
@@ -35,70 +33,45 @@ class App extends Component {
   }
   render() {
     return (
-      <Route render={( {location} ) => (
-        <TransitionGroup>
-          <CSSTransition key={location.key} classNames='fade' timeout={500}>
-            <Switch location={location}>
-              <Route
-                exact
-                path="/palette/new"
-                render={ ( routeProps ) => 
-                  (
-                    <Page>
-                      <NewPaletteForm 
-                      savePalette={this.savePalette} 
-                      {...routeProps} 
-                      palettes={this.state.palettes}
-                      />
-                    </Page>
-                  )
-                }
-              />
-              <Route exact
-                path="/"
-                render={ ( routeProps ) => 
-                  (
-                    <Page>
-                      <PaletteList 
-                      palettes={this.state.palettes} 
-                      deletePalette={this.deletePalette}
-                      {...routeProps} 
-                      />
-                    </Page>
-                  )
-                }
-              />
-              <Route
-                exact
-                path="/palette/:id"
-                render={ ( routeProps ) => 
-                  (
-                    <Page>
-                      <Palette
-                        palette={generatePalette(this.findPalette(routeProps.match.params.id))}
-                      />
-                    </Page>
-                  )
-                }
-              />
-              <Route
-                path={"/palette/:paletteId/:colorId"}
-                render={ ( routeProps ) => 
-                  (
-                    <Page>
-                      <SingleColorPalette
-                        colorId={routeProps.match.params.colorId}
-                        palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
-                      />
-                    </Page>
-                  )
-                }
-              />
-            </Switch>  
-          </CSSTransition>
-        </TransitionGroup>
-      )
-      }/>
+      <Switch>
+        <Route
+          exact
+          path="/palette/new"
+          render={(routeProps) => 
+            (<NewPaletteForm 
+              savePalette={this.savePalette} 
+              {...routeProps} 
+              palettes={this.state.palettes}
+            />)
+        }
+        />
+        <Route exact
+          path="/"
+          render={(routeProps) => <PaletteList 
+            palettes={this.state.palettes} 
+            deletePalette={this.deletePalette}
+            {...routeProps} 
+          />}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={routeProps =>
+            <Palette
+              palette={generatePalette(this.findPalette(routeProps.match.params.id))}
+            />
+          }
+        />
+        <Route
+          path={"/palette/:paletteId/:colorId"}
+          render={routeProps =>
+            <SingleColorPalette
+              colorId={routeProps.match.params.colorId}
+              palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))}
+            />
+          }
+        />
+      </Switch>
     );
   }
 }
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -4,7 +4,6 @@ import MiniPalette from './MiniPalette'
 import { Link } from 'react-router-dom'
 import styles from './styles/PaletteListStyles'
 import { CSSTransition, TransitionGroup } from "react-transition-group";
-<<<<<<< HEAD
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import List from '@material-ui/core/List';
@@ -16,20 +15,36 @@ import CheckIcon from "@material-ui/icons/Check";
 import CloseIcon from "@material-ui/icons/Close";
 import blue from "@material-ui/core/colors/blue";
 import red from "@material-ui/core/colors/red";
-=======
->>>>>>> parent of d02f223... Added delete confirmation to palette list
+
+
 
 class PaletteList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            deleteDialog: false,
+            deletingId: "",
+        }
+        this.openDialog = this.openDialog.bind(this);
+        this.closeDialog = this.closeDialog.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+    openDialog(id){
+        this.setState({deleteDialog: true, deletingId: id})
+    }
+    closeDialog(){
+        this.setState({deleteDialog: false, deletingId: ""})
+    }
     goToPalette(id){
         this.props.history.push(`/palette/${id}`)
     }
+    handleDelete(){
+        this.props.deletePalette(this.state.deletingId);
+        this.closeDialog();
+    }
     render() {
-<<<<<<< HEAD
         const { deleteDialog } = this.state
-        const { palettes, classes, } = this.props
-=======
-        const { palettes, classes, deletePalette, } = this.props
->>>>>>> parent of d02f223... Added delete confirmation to palette list
+        const { palettes, classes } = this.props
         return(
             <div className={classes.root}>
                 <div className={classes.container}>
@@ -43,7 +58,7 @@ class PaletteList extends Component{
                             <MiniPalette 
                                 {...palette} 
                                 handleClick={() => this.goToPalette(palette.id)}
-                                handleDelete={deletePalette}
+                                openDialog={this.openDialog}
                                 key={palette.id}
                                 id={palette.id}
                             />    
@@ -51,6 +66,35 @@ class PaletteList extends Component{
                         ))}   
                     </TransitionGroup>
                 </div>
+                <Dialog 
+                    onClose={this.closeDialog}
+                    open={deleteDialog} 
+                    aria-labelledby='delete-dialog-title'
+                >
+                    <DialogTitle id='delete-dialog-title'>Delete This Palette</DialogTitle>
+                    <List>
+                        <ListItem button onClick={this.handleDelete}>
+                            <ListItemAvatar>
+                                <Avatar style={{backgroundColor: blue[100], color: blue[600]}}>
+                                    <CheckIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>
+                                Delete
+                            </ListItemText>
+                        </ListItem>
+                        <ListItem button onClick={this.closeDialog}>
+                            <ListItemAvatar>
+                                <Avatar style={{backgroundColor: red[100], color: red[600]}}>
+                                    <CloseIcon/>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>
+                                Cancel
+                            </ListItemText>
+                        </ListItem>
+                    </List>
+                </Dialog>
             </div>
         )
     }
