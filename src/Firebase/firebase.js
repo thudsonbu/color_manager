8095f96d1a5d96d20918085ff5a4791ca4e96783
thudsonbu <hudson_thom@bentley.edu>
2020-11-16 22:12:11 -0500

    removed and reset api security

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -1,16 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyA4fxBa0cZ7ixICby18NdSgbhc3ySHfT5A",
-    authDomain: "color-manager.firebaseapp.com",
-    databaseURL: "https://color-manager.firebaseio.com",
-    projectId: "color-manager",
-    storageBucket: "color-manager.appspot.com",
-    messagingSenderId: "570569969501",
-    appId: "1:570569969501:web:fb78e46001ae766d94e2b4",
-    measurementId: "G-QWJQZED2E9"
-};
+import firebaseConfig from './firebaseconf';
 
 class Firebase {
     constructor() {
